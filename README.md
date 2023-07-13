b997aaea0481dd84c31b72911bf642be5dda21d9
airtonsena10 <airtonsena41@gmail.com>
2023-07-13 20:24:50 -0300

    github-blog  com tailwind

---
+++ README.md
@@ -0,0 +1,2 @@
+
+![capa](https://i.imgur.com/4U1FKJp.png)
---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <div className="text-base-label text-xs text-center m-8">
+      Developed by{' '}
+      <a
+        href="https://github.com/airtonsena10"
+        className="border-b border-transparent hover:border-base-label transition-colors"
+        target="_blank"
+        rel="noreferrer"
+      >
+        AirtonSena Dantas
+      </a>{' '}
+      - 2023
+    </div>
+  )
+}
